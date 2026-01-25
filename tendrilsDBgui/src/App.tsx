2e9f5c9a57c9a24907cd4fa48bf43cf16c48dd49
Piyush Saini <sainipiyush0322@gmail.com>
2026-01-25 12:13:30 +0530

    nm

--- tendrilsDBgui/src/App.tsx
+++ tendrilsDBgui/src/App.tsx
@@ -7,7 +7,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter basename="/DB-GUI-TENDRILS">
+    <BrowserRouter basename="/NewDBGui">
       <Routes>
         <Route path="/" element={<Index />} />
         {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
